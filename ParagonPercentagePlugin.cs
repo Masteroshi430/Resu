872c8fb41cebbf0887ea779d2bd5340548bbf756
User5981 <13667502+User5981@users.noreply.github.com>
2018-08-24 12:49:53 +0200

    spam

--- ParagonPercentagePlugin.cs
+++ ParagonPercentagePlugin.cs
@@ -7,6 +7,7 @@ using System.Globalization;
 using Turbo.Plugins.Default;
 using System.Linq;
 
+
 namespace Turbo.Plugins.Resu
 {
     public class ParagonPercentagePlugin : BasePlugin, IInGameTopPainter
