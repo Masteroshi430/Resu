1d55b4db965ec196819576ec84666a48dd62b202
User5981 <13667502+User5981@users.noreply.github.com>
2018-08-24 12:53:30 +0200

    Spam

--- CraftersDelightPlugin.cs
+++ CraftersDelightPlugin.cs
@@ -10,6 +10,7 @@ using System;
  
  
  
+ 
 namespace Turbo.Plugins.Resu
 {
     public class CraftersDelightPlugin : BasePlugin, IInGameWorldPainter, ILootGeneratedHandler, ICustomizer, IInGameTopPainter
