772945bff8e2b3005e0d7ba04c5b5d6f0d04f379
User5981 <13667502+User5981@users.noreply.github.com>
2018-08-24 12:45:30 +0200

    Fuck you

--- DangerPlugin.cs
+++ DangerPlugin.cs
@@ -9,6 +9,7 @@ using Turbo.Plugins.Default;
 using System.Collections.Generic;
 using System.Globalization;
 
+
 namespace Turbo.Plugins.Resu
 {
     public class DangerPlugin : BasePlugin, IInGameWorldPainter
