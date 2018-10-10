2077367e430e68f46d6f3989d3befa420de4e543
User5981 <13667502+User5981@users.noreply.github.com>
2018-10-10 10:28:08 +0200

    Release

---
+++ UnscathedMonsterShowerPlugin.cs
@@ -0,0 +1,49 @@
+//css_reference C:\V7.7.1.dll;
+// https://github.com/User5981/Resu
+// Unscathed Monster Shower plugin for TurboHUD version 10/10/2018 10:26
+using Turbo.Plugins.Default;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace Turbo.Plugins.Resu
+{
+
+    public class UnscathedMonsterShowerPlugin : BasePlugin, IInGameWorldPainter
+    {
+        public WorldDecoratorCollection UnscathedMonsterDecorator { get; set; }
+        public static HashSet<string> DangerousMonsters = new HashSet<string>() {"Wood Wraith", "Highland Walker", "The Old Man", "Fallen Lunatic", "Deranged Fallen", "Fallen Maniac", "Frenzied Lunatic", "Herald of Pestilence", "Terror Demon", "Demented Fallen", "Savage Beast", "Tusked Bogan", "Punisher", "Anarch", "Corrupted Angel", "Winged Assassin", "Exarch"};
+        
+        
+        public UnscathedMonsterShowerPlugin()
+        {
+            Enabled = true;
+        }
+        
+        public override void Load(IController hud)
+        {
+            base.Load(hud);
+            
+            UnscathedMonsterDecorator = new WorldDecoratorCollection(
+            new GroundLabelDecorator(Hud)
+            {
+                TextFont = Hud.Render.CreateFont("tahoma", 6, 255, 255, 0, 0, true, false, 0, 255, 255, 0, false)
+            }
+            );
+            
+        }
+        
+        public void PaintWorld(WorldLayer layer)
+        {
+         double MyMaxWeaponRange = 130D;
+         var UnscathedMonsters = Hud.Game.AliveMonsters.Where(x => x.MaxHealth == x.CurHealth && x.Rarity == ActorRarity.Normal && !x.Untargetable && !x.Invisible && !DangerousMonsters.Contains(x.SnoActor.NameEnglish) &&x.NormalizedXyDistanceToMe < MyMaxWeaponRange);
+         foreach (var UnscathedMonster in UnscathedMonsters)
+         {
+          UnscathedMonsterDecorator.Paint(layer, UnscathedMonster, UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)), "\u2B2C \u2B2C"); // round \u2022 eye \uD83D\uDC41
+         }
+        
+        }
+
+    }
+
+}
