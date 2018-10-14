1056c36c0c8c2d1bd7ac425d44798b3bcdd5aa9b
User5981 <13667502+User5981@users.noreply.github.com>
2018-10-14 08:09:39 +0200

    eye blinkng closer to what the human eye does

--- UnscathedMonsterShowerPlugin.cs
+++ UnscathedMonsterShowerPlugin.cs
@@ -1,6 +1,6 @@
 //css_reference C:\V7.7.1.dll;
 // https://github.com/User5981/Resu
-// Unscathed Monster Shower plugin for TurboHUD version 12/10/2018 11:48
+// Unscathed Monster Shower plugin for TurboHUD version 14/10/2018 08:07
 using Turbo.Plugins.Default;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,7 +39,7 @@ namespace Turbo.Plugins.Resu
          var UnscathedMonsters = Hud.Game.AliveMonsters.Where(x => x.MaxHealth == x.CurHealth && x.Rarity == ActorRarity.Normal && !x.Untargetable && !x.Invisible && !DangerousMonsters.Contains(x.SnoActor.NameEnglish) &&x.NormalizedXyDistanceToMe < MyMaxWeaponRange);
          foreach (var UnscathedMonster in UnscathedMonsters)
          {
-          if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 100 == 0){}
+          if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 40 == 0){}
           else if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 99 == 0) UnscathedMonsterDecorator.Paint(layer, UnscathedMonster, UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)), "   \u2B2C");
           else UnscathedMonsterDecorator.Paint(layer, UnscathedMonster, UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)), "\u2B2C \u2B2C"); // round \u2022 eye \uD83D\uDC41
          }
