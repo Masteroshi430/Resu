d821d8487e6b9494c1f5d34ed3cc4a30f93be741
User5981 <13667502+User5981@users.noreply.github.com>
2018-10-28 02:14:46 +0200

    Better Eyes :-)

--- UnscathedMonsterShowerPlugin.cs
+++ UnscathedMonsterShowerPlugin.cs
@@ -1,6 +1,6 @@
 //css_reference C:\V7.7.1.dll;
 // https://github.com/User5981/Resu
-// Unscathed Monster Shower plugin for TurboHUD version 14/10/2018 08:07
+// Unscathed Monster Shower plugin for TurboHUD version 28/10/2018 02:13
 using Turbo.Plugins.Default;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +11,7 @@ namespace Turbo.Plugins.Resu
 
     public class UnscathedMonsterShowerPlugin : BasePlugin, IInGameWorldPainter
     {
-        public WorldDecoratorCollection UnscathedMonsterDecorator { get; set; }
         public static HashSet<string> DangerousMonsters = new HashSet<string>() {"Wood Wraith", "Highland Walker", "The Old Man", "Fallen Lunatic", "Deranged Fallen", "Fallen Maniac", "Frenzied Lunatic", "Herald of Pestilence", "Terror Demon", "Demented Fallen", "Savage Beast", "Tusked Bogan", "Punisher", "Anarch", "Corrupted Angel", "Winged Assassin", "Exarch"};
-        
         
         public UnscathedMonsterShowerPlugin()
         {
@@ -24,24 +22,42 @@ namespace Turbo.Plugins.Resu
         {
             base.Load(hud);
             
-            UnscathedMonsterDecorator = new WorldDecoratorCollection(
-            new GroundLabelDecorator(Hud)
-            {
-                TextFont = Hud.Render.CreateFont("Segoe UI Symbol", 6, 255, 255, 0, 0, true, false, 0, 255, 255, 0, false)
-            }
-            );
             
         }
         
         public void PaintWorld(WorldLayer layer)
         {
          double MyMaxWeaponRange = 130D;
-         var UnscathedMonsters = Hud.Game.AliveMonsters.Where(x => x.MaxHealth == x.CurHealth && x.Rarity == ActorRarity.Normal && !x.Untargetable && !x.Invisible && !DangerousMonsters.Contains(x.SnoActor.NameEnglish) && x.NormalizedXyDistanceToMe < MyMaxWeaponRange);
+         var UnscathedMonsters = Hud.Game.AliveMonsters.Where(x => x.Rarity == ActorRarity.Normal && !x.Untargetable && !x.Invisible && x.NormalizedXyDistanceToMe < MyMaxWeaponRange); //&& !DangerousMonsters.Contains(x.SnoActor.NameEnglish) 
+         var CatEye = Hud.Texture.GetTexture(2789104100);
+         var BlueEye = Hud.Texture.GetTexture(1423609272);
+         var RedEye = Hud.Texture.GetTexture(2189544651);
+         var BlackEye = Hud.Texture.GetTexture(3379382182);
+         var Eye = Hud.Texture.GetTexture(2789104100);
+         
          foreach (var UnscathedMonster in UnscathedMonsters)
          {
-          if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 40 == 0){}
-          else if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 99 == 0) UnscathedMonsterDecorator.Paint(layer, UnscathedMonster, UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)), "   \u2B2C");
-          else UnscathedMonsterDecorator.Paint(layer, UnscathedMonster, UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)), "\u2B2C \u2B2C"); // round \u2022 eye \uD83D\uDC41
+          var ScreenCoor = UnscathedMonster.FloorCoordinate.Offset(0, 0, (UnscathedMonster.RadiusScaled*3)).ToScreenCoordinate();
+          float Size = (float)((UnscathedMonster.CurHealth/UnscathedMonster.MaxHealth*100)/10);
+          
+          if (UnscathedMonster.SnoActor.Sno % 5 == 0) Eye = RedEye;
+          else if (UnscathedMonster.SnoActor.Sno % 3 == 0) Eye = BlueEye;
+          else Eye = CatEye;
+              
+            if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 40 == 0){}
+            else if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 99 == 0) 
+             {
+             Eye.Draw(ScreenCoor.X-5, ScreenCoor.Y, 10,Size, 1f);
+             }
+            else if ((UnscathedMonster.AcdId + (Hud.Time.Now.Millisecond/2)) % 111 == 0) 
+             {
+             Eye.Draw(ScreenCoor.X+5, ScreenCoor.Y, 10, Size, 1f);
+             }
+            else
+             {
+              Eye.Draw(ScreenCoor.X-5, ScreenCoor.Y, 10, Size, 1f);
+              Eye.Draw(ScreenCoor.X+5, ScreenCoor.Y, 10, Size, 1f);
+             }
          }
         
         }
