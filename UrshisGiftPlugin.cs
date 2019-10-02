aa7d091ff2a9d77d8ece7f7c234f8d31d53ff237
User5981 <13667502+User5981@users.noreply.github.com>
2019-10-02 07:47:11 +0200

    percentage of chance to upgrade in the legendary gem upgrade panel

--- UrshisGiftPlugin.cs
+++ UrshisGiftPlugin.cs
@@ -1,5 +1,5 @@
 ﻿// https://github.com/User5981/Resu
-// Urshi's gift plugin for TurboHUD version 21/08/2019 23:20
+// Urshi's gift plugin for TurboHUD version 02/10/2019 07:44
 
 using System;
 using System.Globalization;
@@ -52,6 +52,56 @@ namespace Turbo.Plugins.Resu
         }
 
         private readonly HashSet<int> Chances = new HashSet<int> { 100, 90, 80, 70, 60, 30, 15, 8, 4, 2, 1 };
+        private readonly List<string> Slots = new List<string> {"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow0._item0.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow0._item1.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow0._item2.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow0._item3.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow0._item4.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow1._item10.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow1._item6.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow1._item7.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow1._item8.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow1._item9.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow2._item12.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow2._item13.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow2._item14.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow2._item15.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow2._item16.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow3._item18.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow3._item19.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow3._item20.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow3._item21.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow3._item22.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow4._item24.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow4._item25.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow4._item26.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow4._item27.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow4._item28.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow5._item30.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow5._item31.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow5._item32.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow5._item33.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow5._item34.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow6._item36.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow6._item37.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow6._item38.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow6._item39.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow6._item40.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow7._item42.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow7._item43.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow7._item44.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow7._item45.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow7._item46.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow8._item48.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow8._item49.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow8._item50.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow8._item51.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow8._item52.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow9._item54.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow9._item55.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow9._item56.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow9._item57.Item",
+"Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content._stackpanel._tilerow9._item58.Item"};
 
         public Func<float> LeftFunc { get; set; }
         public Func<float> TopFunc { get; set; }
@@ -106,6 +156,21 @@ namespace Turbo.Plugins.Resu
                 return;      //false settings
             if (NumberOfAttempts < 1 || NumberOfAttempts > 5)
                 return;   //false settings
+
+            var UpgradeGemPanel =  Hud.Render.GetUiElement("Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content");
+            if (UpgradeGemPanel.Visible)
+             {
+                var UpgradeRect = UpgradeGemPanel.Rectangle;
+
+                foreach (var Slot in Slots)
+                {
+                 var datSlot = Hud.Render.GetUiElement(Slot);
+                 var Item = Hud.Game.Items.FirstOrDefault(x => x.AcdId == datSlot.LegendaryGemAcdId);
+                 if (Item == null || !datSlot.Visible) continue;
+                 else DrawItemChancePercentage(Item, datSlot.Rectangle);
+                }
+            }
+
 
             if (clipState == ClipState.Inventory && InventoryNumbers)
             {
@@ -180,6 +245,89 @@ namespace Turbo.Plugins.Resu
                 var text = GRlevel.ToString("D", CultureInfo.InvariantCulture);
                 var layout = GRupgradeChanceFont.GetTextLayout(text);
                 GRupgradeChanceFont.DrawText(layout, rect.Right - layout.Metrics.Width - 3, rect.Bottom - layout.Metrics.Height - 3);
+
+            }
+        }
+
+        private void DrawItemChancePercentage(IItem item, System.Drawing.RectangleF rect)
+        {
+            var jewelRank = item.JewelRank;
+            if (jewelRank == -1)
+            { jewelRank = 0; }
+            bool Max = IsMaxedGem(item, jewelRank);
+            var UpgradeGemPanelRect = Hud.Render.GetUiElement("Root.NormalLayer.vendor_dialog_mainPage.riftReward_dialog.LayoutRoot.gemUpgradePane.items_list._content").Rectangle;
+
+           if (Max)
+            {
+                var layout = GRupgradeChanceFont.GetTextLayout("max");
+                if ((rect.Bottom - layout.Metrics.Height - 3) > UpgradeGemPanelRect.Top && (rect.Bottom - layout.Metrics.Height - 3) < UpgradeGemPanelRect.Bottom)
+                 {
+                  GRupgradeChanceFont.DrawText(layout, rect.Right - layout.Metrics.Width - 3, rect.Bottom - layout.Metrics.Height - 3);
+                 }
+                return;
+            }
+
+            else
+            {
+                uint DatGRlevel = Hud.Game.Me.InGreaterRiftRank;
+                int Diff = (int)(DatGRlevel - jewelRank);
+
+                if (Diff > 10) Diff = 10;
+                else if (Diff < -7) Diff = -7;
+
+                string Percentage = string.Empty;
+
+                switch (Diff)
+                {
+                    case 10:
+                        Percentage = "100%";
+                        break;
+                    case 9:
+                        Percentage = "90%";
+                        break;
+                    case 8:
+                        Percentage = "80%";
+                        break;
+                    case 7:
+                        Percentage = "70%";
+                        break;
+                    case 6:
+                    case 5:
+                    case 4:
+                    case 3:
+                    case 2:
+                    case 1:
+                    case 0:
+                        Percentage = "60%";
+                        break;
+                    case -1:
+                        Percentage = "30%";
+                        break;
+                    case -2:
+                        Percentage = "15%";
+                        break;
+                    case -3:
+                        Percentage = "8%";
+                        break;
+                    case -4:
+                        Percentage = "4%";
+                        break;
+                    case -5:
+                        Percentage = "2%";
+                        break;
+                    case -6:
+                        Percentage = "1%";
+                        break;
+                    case -7:
+                        Percentage = "0%";
+                        break;
+                }
+
+                var layout = GRupgradeChanceFont.GetTextLayout(Percentage);
+                if ((rect.Bottom - layout.Metrics.Height - 3) > UpgradeGemPanelRect.Top && (rect.Bottom - layout.Metrics.Height - 3) < UpgradeGemPanelRect.Bottom)
+                {
+                    GRupgradeChanceFont.DrawText(layout, rect.Right - layout.Metrics.Width - 3, rect.Bottom - layout.Metrics.Height - 3);
+                }
 
             }
         }
